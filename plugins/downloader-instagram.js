48f12430dc14d2a3bbc95fe783d9ae01643921c6
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-11-22 20:11:00 -0500

    Update downloader-instagram.js

--- plugins/downloader-instagram.js
+++ plugins/downloader-instagram.js
@@ -1,28 +1,49 @@
-import fetch from 'node-fetch'
+import axios from 'axios'
 
 let handler = async (m, { conn, args }) => {
-    if (!args[0]) throw m.reply('✧ Ingresa el link de *Instagram*')
+    if (!args[0]) throw m.reply('Ingresa el link de Instagram');
     const sender = m.sender.split('@')[0];
     const url = args[0];
 
-try {
-    let res = await fetch(global.API('fgmods', '/api/downloader/igdl', { url: args[0] }, 'apikey'))
-    if (!res.ok) throw `error` 
-    let data = await res.json()
+    m.reply(wait);
 
-    for (let item of data.result) {
-        conn.sendFile(m.chat, item.url, 'igdl.jpg', `enviado..`, m)
-    }
+    try {
+        const { data } = await axios.get(`fgmods', '/api/downloader/igdl', { url: args[0] }, 'apikey`);
 
+        if (!data.status || !data.data || data.data.length === 0) throw m.reply('Error');
 
+        // Prioritize 720p (HD) and fallback to 360p (SD)
+        let video = data.results[0];
+        
+        if (video && video.url) {
+            const videoBuffer = await axios.get(video.url, { responseType: 'arraybuffer' }).then(res => res.data);
+            const caption = `✧ Para: @${sender}`;
+
+            await conn.sendMessage(
+                m.chat, {
+                video: videoBuffer,
+                mimetype: "video/mp4",
+                fileName: `video.mp4`,
+                caption: caption,
+                mentions: [m.sender],
+            }, {
+                quoted: m
+            }
+            );
+        } else {
+            throw m.reply('Error');
+        }
     } catch (error) {
-        m.reply(`error`)
+        console.error('Handler Error:', error);
+        conn.reply(m.chat, `Error: ${error}`, m);
     }
-
 }
-handler.help = ['instagram <link ig>']
-handler.tags = ['dl']
-handler.command = ['ig', 'igdl', 'instagram', 'igimg', 'igvid']
-handler.diamond = true
+
+handler.help = ['ig'].map(v => v + ' *<link>*')
+handler.tags = ['downloader']
+
+handler.command = /^(ig(dl)?)$/i
+
+handler.register = true
 
 export default handler
